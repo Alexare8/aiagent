3286e34e90d02dff2f347009b99198b3b248a726
alexare8 <duncorr@gmail.com>
2025-10-30 06:14:17 -0400

    accept prompt from command line, CH1L4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -8,7 +8,11 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if len(sys.argv) != 2:
+        sys.exit("invalid arguments")
+    request = sys.argv[1]
+
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=request)
 
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
