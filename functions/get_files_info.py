3c72f5ae3ff74a211224a736e50e9b20b993028b
alexare8 <duncorr@gmail.com>
2025-10-31 22:47:32 -0400

    create function declaration to let model know what it can do, CH3L2

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 from functions.sanitize_path import sanitize_path
 
 def get_files_info(working_directory, directory="."):
@@ -16,3 +17,17 @@ def get_files_info(working_directory, directory="."):
         file_descriptions.append(f" - {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}")
 
     return "\n".join(file_descriptions)
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import os, sys
+import os, sys, types
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from config import system_prompt
+from functions.get_files_info import schema_get_files_info
 
 def main():
     load_dotenv()
@@ -16,13 +17,17 @@ def main():
     user_prompt = sys.argv[1]
 
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    available_functions = types.Tool(function_declarations=[schema_get_files_info,])
+    config = genai.types.GenerateContentConfig(system_instruction=system_prompt, tools=[available_functions])
 
-    config = genai.types.GenerateContentConfig(system_instruction=system_prompt)
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=config)
 
     if verbose:
         print(f"User prompt: {user_prompt}")
     print(response.text)
+    if len(response.function_calls) > 0:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
